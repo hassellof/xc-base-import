9e0632ddc127c2043b8085e4e81ed4e3d25d8f2a
Daniil Konoplev <danchoys@icloud.com>
2017-09-16 13:55:29 +0300

    Actually call the importer

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var importer = require('./lib/importer');
 
 program
   .option('-x, --xliff <xliff file location>', 'The XLIFF file to import')
   .option('-p, --project <xcode project location>', 'The location of the project to import the file into')
   .parse(process.argv);
 
-console.log('xliff: %s; project: %s; file: %s', program.xliff, program.project);
+importer.importLocalization(program.xliff, program.project);
