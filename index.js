2c43223e5f2b74c95726a75dad8418ce022b7308
Daniil Konoplev <danchoys@icloud.com>
2017-09-16 13:48:32 +0300

    Implement the basic setup for the localization

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var program = require('commander');
 
-
 program
   .option('-x, --xliff <xliff file location>', 'The XLIFF file to import')
   .option('-p, --project <xcode project location>', 'The location of the project to import the file into')
